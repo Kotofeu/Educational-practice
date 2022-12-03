30d6ea6c3b273b2acbfd5903fcc011e7d9c8a0d3
Petrov Danil <cras.petrov@yandex.ru>
2022-12-03 21:57:38 +0200

    Start

--- Неделя 1 Питон/Task 5 variant 15.py
+++
@@ -1,8 +0,0 @@
-coast = float(input("Введите стоимость слова: "))
-words = input("Введите телеграмму: ").split()
-result = len(words) * coast
-print("Квитанция об оплате:", '\n',
-      'Количество слов:', len(words), '\n',
-      "Стоимость слова:", coast, '\n',
-      "Оплата:", result) 
-input()
